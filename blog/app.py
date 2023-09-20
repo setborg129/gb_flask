544681a8dbe63097e53f855b32cba0717f253834
max <setborg1@inbox.ru>
2023-09-20 15:18:27 +0500

    lesson10

--- blog/app.py
+++ blog/app.py
@@ -146,16 +146,16 @@ def create_tags():
     """
     from blog.models import Tag
     for name in [
-    "flask",
-    "django",
-    "python",
-    "sqlalchemy",
-    "news",
-    ]:
-    tag = Tag(name=name)
-    db.session.add(tag)
-    db.session.commit()
-    print("created tags")
+            "flask",
+            "django",
+            "python",
+            "sqlalchemy",
+            "news",
+        ]:
+        tag = Tag(name=name)
+        db.session.add(tag)
+        db.session.commit()
+        print("created tags")
 
 app.register_blueprint(users_app, url_prefix="/users")
 
